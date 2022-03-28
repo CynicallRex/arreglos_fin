f88bcbe9ef89ddfd580f5393e01f0c850700e681
cynicallrex <nawelmisha@gmail.com>
2022-03-28 12:33:51 -0400

    INICIO DE PROYECTO

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        //Practica_1 practica_1 = new Practica_1();
+        //practica_1.ejecutar();
+        Practica_2 practica_2 = new Practica_2();
+        practica_2.ejecutar();
+
+    }
+}
